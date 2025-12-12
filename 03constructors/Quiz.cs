075cbc94c03ea1390b1560dbf7303d9e9de75634
Averon Dekker <41345@ma-web.nl>
2025-12-12 13:06:43 +0100

    3de week 03

--- 03constructors/Quiz.cs
+++ 03constructors/Quiz.cs
@@ -27,24 +27,35 @@
             vragen = new QuizVraag[aantalVragen];
             ingevuldeAntwoorden = new QuizVraagAntwoord[aantalVragen];
         }
+
+        internal void VoegVraagToeOpIndex(int index, QuizVraag vraag)
+        {
+            vragen[index] = vraag;
+        }
+
+        internal void VoegVraagToeOpIndex(int index, string vraagText, string antwoord)
+        {
+            QuizVraag nieuweVraag = new QuizVraag(vraagText, antwoord);
+            VoegVraagToeOpIndex(index, nieuweVraag);
+        }
     }
 
     class Program
     {
         private void Run()
         {
-            Quiz quiz = new Quiz(10);   // quiz met 10 vragen
+            Quiz quiz = new Quiz(10);
 
-            quiz.vragen[0] = new QuizVraag("Wat is de hoofdstad van Nederland?", "Amsterdam");
-            quiz.vragen[1] = new QuizVraag("Welke kleur krijg je door blauw en geel te mengen?", "Groen");
-            quiz.vragen[2] = new QuizVraag("Hoeveel dagen heeft een week?", "7");
-            quiz.vragen[3] = new QuizVraag("Wat is het grootste land ter wereld?", "Rusland");
-            quiz.vragen[4] = new QuizVraag("Hoeveel poten heeft een spin?", "8");
-            quiz.vragen[5] = new QuizVraag("Wat is de som van 12 + 8?", "20");
-            quiz.vragen[6] = new QuizVraag("Welke planeet staat het dichtst bij de zon?", "Mercurius");
-            quiz.vragen[7] = new QuizVraag("Hoe heet de grootste oceaan?", "Stille Oceaan");
-            quiz.vragen[8] = new QuizVraag("Hoeveel seconden zitten er in een minuut?", "60");
-            quiz.vragen[9] = new QuizVraag("Wat is de hoofdstad van België?", "Brussel");
+            quiz.VoegVraagToeOpIndex(0, "Wat is de hoofdstad van Nederland?", "Amsterdam");
+            quiz.VoegVraagToeOpIndex(1, "Welke kleur krijg je door blauw en geel te mengen?", "Groen");
+            quiz.VoegVraagToeOpIndex(2, "Hoeveel dagen heeft een week?", "7");
+            quiz.VoegVraagToeOpIndex(3, "Wat is het grootste land ter wereld?", "Rusland");
+            quiz.VoegVraagToeOpIndex(4, "Hoeveel poten heeft een spin?", "8");
+            quiz.VoegVraagToeOpIndex(5, new QuizVraag("Wat is de som van 12 + 8?", "20"));
+            quiz.VoegVraagToeOpIndex(6, new QuizVraag("Welke planeet staat het dichtst bij de zon?", "Mercurius"));
+            quiz.VoegVraagToeOpIndex(7, new QuizVraag("Hoe heet de grootste oceaan?", "Stille Oceaan"));
+            quiz.VoegVraagToeOpIndex(8, new QuizVraag("Hoeveel seconden zitten er in een minuut?", "60"));
+            quiz.VoegVraagToeOpIndex(9, new QuizVraag("Wat is de hoofdstad van België?", "Brussel"));
         }
 
         static void Main()
